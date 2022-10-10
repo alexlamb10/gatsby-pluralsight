de8f6971957ad0039d10d4aba7f576cf635787f0
Alex Lamb <alexlamb104@gmail.com>
2022-10-10 09:14:22 -0600

    Added Links between pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-export default function Home() {
+export default function About() {
     return <div>
-        <h1>This is the index page</h1>
+        <h1>This is the About page</h1>
         <div>
             <a href="/">Home</a> <a href="/about">About</a>
         </div>
