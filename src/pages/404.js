efc4b6e4006164517ce9267e98330d4a277357c1
Alex Lamb <alexlamb104@gmail.com>
2022-10-26 10:24:04 -0600

    Running into styling errors

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,11 @@
 import React from "react";
+import styles from '404.module.scss'
 
 export default function handleError() {
-    return <div>
-        <h1>Page not found</h1>
-        <p>The page you are looking for does not exist</p>
+    return <div className={styles.content}>
+        <h1 className={styles.headerMessage}>Page not found</h1>
+        <p className={styles.errorMessage}>
+            The page you are looking for does not exist
+        </p>
     </div>
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function About() {
     return <div>
         <h1>This is the About page</h1>
         <div>
-            <a href="/">Home</a> <a href="/about">About</a>
+        <Link to="/">Home</Link> <Link to="/about">About</Link>
         </div>
         <p>Here is some more text for the page</p>
     </div>
