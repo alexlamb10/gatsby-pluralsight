599aedda26820bdbbabf9fc66371574fce5384ba
Alex Lamb <alexlamb104@gmail.com>
2022-10-31 15:54:26 -0600

    Fixed styling issue

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from '404.module.scss'
+import * as styles from './errorMsg.module.scss'
 
 export default function handleError() {
     return <div className={styles.content}>
-        <h1 className={styles.headerMessage}>Page not found</h1>
+        <h1 className={styles.header}>Page not found</h1>
         <p className={styles.errorMessage}>
             The page you are looking for does not exist
         </p>
