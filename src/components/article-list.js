9ac228e015aa46f76a915d375e8121893b168c36
Alex Lamb <alexlamb104@gmail.com>
2022-11-07 16:01:32 -0700

    Cleaned up the for the posts

---
+++ src/components/article-list.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Article from "./article";
+
+export default () => (
+    <StaticQuery 
+    query={ graphql ` {
+        allMarkdownRemark(sort:{fields: [frontmatter___date],order:DESC}){
+          totalCount
+          edges{
+            node{
+              id
+              frontmatter{
+                title
+                image
+                date(formatString: "MMMM YYYY")
+                keywords
+              }
+              excerpt
+            }
+          }
+        }
+      }`
+      
+
+    }
+
+    render = {data => (
+        <div>
+            {data.allMarkdownRemark.edges.map(({node}) => (
+                <Article 
+                    to='/'
+                    keywords={node.frontmatter.keywords}
+                    title={node.frontmatter.title}
+                    date={node.frontmatter.date}
+                    excerpt={node.excerpt}
+                />
+            ))}
+        </div>
+    )}
+    />
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,13 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Title from "../components/title"
-import Article from "../components/article"
+import ArticleList from "../components/article-list"
 
-export default function Home({data}) {
+export default function Home() {
     return <Layout>
         <Title text='Welcome' />
-        <div>
-            <Link to="/">Home</Link> <Link to="/about">About</Link>
-        </div>
         <p>Here is some more text for the page</p>
 
-        <div>
-            {data.allMarkdownRemark.edges.map(({node}) => (
-                <Article 
-                    to='/'
-                    keywords={node.frontmatter.keywords}
-                    title={node.frontmatter.title}
-                    date={node.frontmatter.date}
-                    excerpt={node.excerpt}
-                />
-            ))}
-        </div>
+        <ArticleList />
     </Layout>
 }
-
-export const query = graphql ` {
-    allMarkdownRemark(sort:{fields: [frontmatter___date],order:DESC}){
-      totalCount
-      edges{
-        node{
-          id
-          frontmatter{
-            title
-            image
-            date(formatString: "MMMM YYYY")
-            keywords
-          }
-          excerpt
-        }
-      }
-    }
-  }`
-  
