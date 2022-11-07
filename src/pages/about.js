4b1303019c68e6117f01e5b3817e83fed95ab24d
Alex Lamb <alexlamb104@gmail.com>
2022-11-07 09:05:31 -0700

    Added more components that are reusable

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Title from "../components/title"
 
 export default function About() {
     return <Layout>
-        <h1>This is the About page</h1>
+        <Title text='About Me' />
         <div>
         <Link to="/">Home</Link> <Link to="/about">About</Link>
         </div>
