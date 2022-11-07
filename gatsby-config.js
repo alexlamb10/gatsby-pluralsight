fb468783dfba055cc45f705988ac6bff825d4655
Alex Lamb <alexlamb104@gmail.com>
2022-11-07 15:02:29 -0700

    Used graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+
+  siteMetadata:{
+    title: 'Gatsby Blog'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Title from "../components/title"
 
-export default function Home() {
+export default function Home({data}) {
     return <Layout>
-        <Title text='Welcome' />
+        <Title text={data.site.siteMetadata.title} />
         <div>
             <Link to="/">Home</Link> <Link to="/about">About</Link>
         </div>
         <p>Here is some more text for the page</p>
     </Layout>
 }
+
+export const query = graphql `query {
+    site{
+        siteMetadata{
+            title
+        }
+    }
+}`
